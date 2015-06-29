ba16320b74077d1fa22dd47f813aebea1671b0ac
Alex Atmore <acatmore@gmail.com>
2015-06-29 19:34:58 +0000

    added jquery animations to landing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,1 @@
- $(document).ready(function() { 
-   $('.hero-content h3').click(function(){
-      var subText = $(this).text();
-      $(this).text(subText + "!");
-   });
-});
+ require("./landing");
