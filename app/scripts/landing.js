095328767a3434f2e3a5620694d3971fcf069b4b
Alex Atmore <acatmore@gmail.com>
2015-06-29 20:05:48 +0000

    added hover event for font size

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -7,12 +7,13 @@
    var onHoverAction = function(event) {
      console.log('Hover action triggered.');
      $(this).animate({'margin-top': '10px'});
+     $(this).animate({'font-size': '200px'});
    };
  
    var offHoverAction = function(event) {
      console.log('Off-hover action triggered.');
      $(this).animate({'margin-top': '0px'});
+     $(this).animate({'font-size': '70px'})
    };
- 
    $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
