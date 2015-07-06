fe00478b052c5ba908131789db7aade2a9ba02bb
Alex Atmore <acatmore@gmail.com>
2015-07-06 21:14:55 +0000

    added a profile and subscription page with jquery tabs

---
+++ app/scripts/profile.js
@@ -0,0 +1,20 @@
+var tabsContainer = ".user-profi;e-tabs-container"
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + "li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $(".tab-pane").addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+
+};
+
+if (document.URL.match(/\/profile.html/)) {
+  $(document).ready(function() {
+    var $tabs = $(tabsContainer + " a");
+    $tabs.click(selectTabHandler);
+    $tabs[0].click();
+  });
+}
