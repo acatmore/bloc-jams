3b066dd339bd3bbb9a5f7715438021bfe0f56d4f
Alex Atmore <acatmore@gmail.com>
2015-07-08 21:51:10 +0000

    converted collection view to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -265,29 +265,54 @@ var currentlyPlayingSong = null;
  // require('./album');
  // require('./profile');
 
+ //example album
+ var albumPicasso = {
+   name: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: '/images/album-placeholder.png',
+   songs: [
+       { name: 'Blue', length: '4:26' },
+       { name: 'Green', length: '3:14' },
+       { name: 'Red', length: '5:01' },
+       { name: 'Pink', length: '3:21'},
+       { name: 'Magenta', length: '2:15'}
+     ]
+ };
+
 blocJams = angular.module('BlocJams', ['ui.router']);
 
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-   $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
  
-   $stateProvider.state('landing', {
-     url: '/',
-     controller: 'Landing.controller',
-     templateUrl: '/templates/landing.html'
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
+
+  $stateProvider.state('song', {
+    url: '/song',
+    controller: 'Song.controller',
+    templateUrl: '/templates/song.html'
+  });
+
+   $stateProvider.state('collection', {
+     url: '/collection',
+     controller: 'Collection.controller',
+     templateUrl: '/templates/collection.html'
    });
-    $stateProvider.state('song', {
-  url: '/',
-  controller: 'song.controller',
-  templateUrl: '/templates/song.html'
-});
  }]);
-
- 
-
-$scope.subText = "Turn the music up!";
  
  // This is a cleaner way to call the controller than crowding it on the module definition.
  blocJams.controller('Landing.controller', ['$scope', function($scope) {
+
+    $scope.subText = "Turn the music up!";
+ 
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
 
    $scope.albumURLs = [
      '/images/album-placeholders/album-1.jpg',
@@ -301,6 +326,13 @@ $scope.subText = "Turn the music up!";
      '/images/album-placeholders/album-9.jpg',
    ];
    
+ }]);
+
+ blocJams.controller('Collection.controller', ['$scope', function($scope) {
+   $scope.albums = [];
+    for (var i = 0; i < 33; i++) {
+      $scope.albums.push(angular.copy(albumPicasso));
+   }
  }]);
 });
 
