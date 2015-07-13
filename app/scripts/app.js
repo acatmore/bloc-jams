b80cce8fe2af66bdc2063cc41691eb46c87f8f8c
Alex Atmore <acatmore@gmail.com>
2015-07-13 20:52:15 +0000

    added a working time indicator

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -109,6 +109,7 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
 
     $scope.playSong = function(song) {
       SongPlayer.setSong($scope.album, song);
+      SongPlayer.play();
     };
  
     $scope.pauseSong = function(song) {
@@ -118,9 +119,16 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
 
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
+
+    SongPlayer.onTimeUpdate(function(event, time){
+     $scope.$apply(function(){
+       $scope.playTime = time;
+     });
+   });
+
  }]);
  
- blocJams.service('SongPlayer', function() {
+ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
   var currentSoundFile = null;
   var trackIndex = function(album, song) {
      return album.songs.indexOf(song);
@@ -167,21 +175,29 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
          currentSoundFile.setTime(time);
        }
      },
+     onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
      setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
       }
       this.currentAlbum = album;
       this.currentSong = song;
+
       currentSoundFile = new buzz.sound(song.audioUrl, {
       formats: [ "mp3" ],
       preload: true
     });
- 
+
+    currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });
+    
     this.play();
      }
    };
- });
+ }]);
 
 
  blocJams.directive('slider', ['$document', function($document){
@@ -278,3 +294,32 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
    };
 
  }]);
+
+ blocJams.filter('timecode', function(){
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ })
